cb8681856d6e6d0cbd79ef9a1a1554fc4b50eb85
Frank Garcia <00frankgarcia@gmail.com>
2021-10-22 17:21:27 -0300

    changes in home view

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as Bar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Image, StatusBar, Platform } from 'react-native';
+import tw from "twrnc"
+import background from "./assets/test/background.jpeg"
+import brand from "./assets/patitapp.png"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ImageBackground source={background} resizeMode="cover" style={styles.image}>
+        <Image source={brand} style={{ height: 160, width: 240 }} />
+        <StatusBar backgroundColor="transparent"/>
+        <Bar style="auto" />
+      </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    paddingTop: Platform && Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
 });
