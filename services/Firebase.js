d66c4ea42a1c2c1b489a52141ddc25d33ac4703b
Frank Garcia <00frankgarcia@gmail.com>
2021-11-10 11:56:32 -0300

    using Firebase

---
+++ services/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwdMucLbfN13i6hO-ryaWVf0t2W-dbuB4",
+  authDomain: "patitapp-dce25.firebaseapp.com",
+  projectId: "patitapp-dce25",
+  storageBucket: "patitapp-dce25.appspot.com",
+  messagingSenderId: "203158461668",
+  appId: "1:203158461668:web:caf737d5434444ae7f20f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
