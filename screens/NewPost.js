b1010cfd65f492949cddc1acb8b3db31ecb0311e
Frank Garcia <00frankgarcia@gmail.com>
2021-11-11 14:19:54 -0300

    new screens/components refactory

---
+++ screens/NewPost.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function NewPost() {
+  return (
+    <View>
+      <View>
+        <Text>Crear aviso de perdido</Text>
+      </View>
+      <View>
+        <Text>Crear aviso de encontrado</Text>
+      </View>
+    </View>
+  )
+}
