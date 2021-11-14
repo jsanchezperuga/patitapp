0ffcbd4a6f7bdd6738e7376707b8373295f6bf3f
Erik Nielsen <eriksoad8@gmail.com>
2021-11-14 18:43:15 -0300

    style changes in WhatsappButton and PostInfo

--- components/PostInfo.js
+++ components/PostInfo.js
@@ -1,46 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { ScrollView, Text, StyleSheet, Image } from 'react-native'
 import WhatsappButton from './WhatsappButton'
-
 
 export default function PostInfo() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>Lola te está buscando</Text>
-      <Image  
-      source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Golden_Retriever_9-year_old.jpg/1200px-Golden_Retriever_9-year_old.jpg'}}
-      style={styles.image}
+      <Image
+        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Golden_Retriever_9-year_old.jpg/1200px-Golden_Retriever_9-year_old.jpg' }}
+        style={styles.image}
       />
       <Text style={styles.row}>Contacto: Ana</Text>
-    <Text style={styles.row}>Zona: Devoto</Text>
-      <Text  style={styles.desc}>Golden Retriever perdida, tiene 5 años y tenia un collar verde, se escapo por la placita Libertad el martes 2 de noviembre. Cualquier info contactarse al whatsapp</Text>
-      <WhatsappButton/>
-
-    </View>
-    
+      <Text style={styles.row}>Zona: Devoto</Text>
+      <Text style={styles.desc}>Golden Retriever perdida, tiene 5 años y tenia un collar verde, se escapo por la placita Libertad el martes 2 de noviembre. Cualquier info contactarse al whatsapp
+      Golden Retriever perdida, tiene 5 años y tenia un collar verde, se escapo por la placita Libertad el martes 2 de noviembre. Cualquier info contactarse al whatsapp
+      Golden Retriever perdida, tiene 5 años y tenia un collar verde, se escapo por la placita Libertad el martes 2 de noviembre. Cualquier info contactarse al whatsapp
+      Golden Retriever perdida, tiene 5 años y tenia un collar verde, se escapo por la placita Libertad el martes 2 de noviembre. Cualquier info contactarse al whatsapp</Text>
+      <WhatsappButton />
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     marginTop: StatusBar.currentHeight || 0,
     backgroundColor: "#a9a9a9",
-    padding: 16
+    padding: 16,
+    marginBottom: 24,
   },
   row: {
     padding: 4,
     borderBottomColor: "white",
     borderBottomWidth: StyleSheet.hairlineWidth,
-    color: 'white'
+    color: 'white',
+    justifyContent: "center",
+    marginTop: 0,
+    marginBottom: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+
   },
   desc: {
     fontSize: 17,
     justifyContent: "center",
     color: '#ffffff',
-    padding: 16
+    padding: 16,
+   
   },
   title: {
     fontSize: 32,
--- components/WhatsappButton.js
+++ components/WhatsappButton.js
@@ -1,41 +1,43 @@
-import iconSet from '@expo/vector-icons/build/Fontisto'
 import React from 'react'
-import { TouchableOpacity, Font,Linking, Text, StyleSheet, Image } from 'react-native'
-
+import { TouchableOpacity, Linking, Text, StyleSheet, Image } from 'react-native'
 
 export default function WhatsappButton() {
-    const openWhatsapp = () => {
-        Linking.openURL('whatsapp://send?phone=+5491155130671') 
-        
-    }
-    return (
-      <TouchableOpacity onPress={openWhatsapp}>
-         <Text style={styles.buttonText}>
+  const openWhatsapp = () => {
+    Linking.openURL('whatsapp://send?phone=+5491155130671')
+  }
+
+  return (
+    <TouchableOpacity style={styles.button} onPress={openWhatsapp}>
+      <Text style={styles.buttonText}>
         <Image source={{ uri: "https://img.icons8.com/color/48/000000/whatsapp--v1.png" }} style={styles.whatsappLogo} />
         Comunicarse al Whatsapp
       </Text>
     </TouchableOpacity>
-           
-    )
+  )
 }
+
 const styles = StyleSheet.create({
-    button: {
-      backgroundColor: "#FFFFFF",
-      padding: 15,
-      paddingTop: 0,
-      borderWidth: 2,
-      borderColor: "#000000",
-      borderRadius: 10,
-      height: 50
-    },
-    buttonText: {
-      color: "white",
-      fontSize: 14,
-      fontWeight: '700',
-      minHeight: 50,
-    },
-    whatsappLogo: {
-      width: 30,
-      height: 30,
-    }
-  })
+  button: {
+    backgroundColor: "#25D366",
+    padding: 15,
+    paddingTop: 0,
+    borderWidth: 2,
+    borderColor: "#000000",
+    borderRadius: 10,
+    height: 50,
+    marginBottom: 35,
+    marginTop: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: '700',
+    minHeight: 50,
+  },
+  whatsappLogo: {
+    width: 30,
+    height: 30,
+  }
+})
