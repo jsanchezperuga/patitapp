458c6b9f5c0ed08df7702850f4fade0190c0fb86
Erik Nielsen <eriksoad8@gmail.com>
2021-11-14 20:13:36 -0300

    wpp fixed

--- components/PostInfo.js
+++ components/PostInfo.js
@@ -4,6 +4,7 @@ import { ScrollView, Text, StyleSheet, Image } from 'react-native'
 import WhatsappButton from './WhatsappButton'
 
 export default function PostInfo({ id, title, image, contactName, desc, zone, wpp, setVisible }) {
+ //usar el el id
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.title} onPress={() => setVisible(false)}>X</Text>
@@ -15,7 +16,7 @@ export default function PostInfo({ id, title, image, contactName, desc, zone, wp
       <Text style={styles.row}>Contacto: {contactName}</Text>
       <Text style={styles.row}>Zona: {zone}</Text>
       <Text style={styles.desc}>{desc}</Text>
-      <WhatsappButton number={wpp} />
+      <WhatsappButton wpp={wpp} />
     </ScrollView>
   )
 }
