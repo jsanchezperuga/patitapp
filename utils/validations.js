9589dac6d8b9d5b8e89ae4f5f5abc72001bd4f49
Frank Garcia <00frankgarcia@gmail.com>
2021-11-16 11:43:28 -0300

    added validations for post creation

--- utils/validations.js
+++ utils/validations.js
@@ -31,3 +31,71 @@ export function validatePassword(password) {
 
   return [error, message]
 }
+
+export function validatePostTitle(title) {
+  let error = false;
+  let message = "";
+  if (title.length === 0) {
+    error = true;
+    message = "El titulo no puede estar vacio";
+  } else if (title.length < 6) {
+    error = true;
+    message = "El titulo es muy corto (min 6 caracteres)";
+  } else if (title.length > 30) {
+    error = true;
+    message = "El titulo es muy largo (max 30 caracteres)"
+  }
+
+  return [error, message]
+}
+
+export function validatePostContactName(contactName) {
+  let error = false;
+  let message = "";
+  if (contactName.length === 0) {
+    error = true;
+    message = "El contacto no puede estar vacio";
+  } else if (contactName.length < 6) {
+    error = true;
+    message = "El contacto es muy corto (min 6 caracteres)";
+  } else if (contactName.length > 20) {
+    error = true;
+    message = "El contacto es muy largo (max 20 caracteres)"
+  }
+
+  return [error, message]
+}
+
+export function validatePostArea(area) {
+  let error = false;
+  let message = "";
+  if (area.length === 0) {
+    error = true;
+    message = "La zona no puede estar vacia";
+  } else if (area.length < 4) {
+    error = true;
+    message = "La zona es muy corta (min 4 caracteres)";
+  } else if (area.length > 42) {
+    error = true;
+    message = "La zona es muy larga (max 42 caracteres)"
+  }
+
+  return [error, message]
+}
+
+export function validatePostContactNumber(contactNumber) {
+  let error = false;
+  let message = "";
+  if (contactNumber.length === 0) {
+    error = true;
+    message = "El numero de contacto no puede estar vacio";
+  } else if (contactNumber.length !== 10) {
+    error = true;
+    message = "El numero de contacto debe tener 10 caracteres";
+  } else if (contactNumber.slice(0, 2) !== "11") {
+    error = true;
+    message = "El numero de contacto debe comenzar con 11"
+  }
+
+  return [error, message]
+}
