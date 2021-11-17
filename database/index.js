4ca1083737abb368da28a6c110fb2fc17f74d2ac
Frank Garcia <00frankgarcia@gmail.com>
2021-11-17 01:08:50 -0300

    displayName comprobation

--- database/index.js
+++ database/index.js
@@ -1,5 +1,8 @@
 import { collection as useCollection, addDoc } from "firebase/firestore";
+import { updateProfile } from "@firebase/auth";
+import { auth } from "../services/Firebase";
 import { db } from "../services/Firebase";
+import { fire } from "react-native-alertbox";
 
 export async function createPost(collection, data) {
   let result = null;
@@ -12,4 +15,29 @@ export async function createPost(collection, data) {
   }
 
   return result;
+}
+
+export async function comprobateDisplayName({ user }) {
+  if ("providerData" in user && user.providerData[0].displayName === null) {
+    return new Promise((res) => {
+      res(fire({
+        title: 'Ingrese un usuario',
+        message: 'Porfavor ingrese un nombre de usuario (OJO! este no podra cambiarse)',
+        actions: [
+          {
+            text: 'Continuar',
+            onPress: async ({ name }) => {
+              await updateProfile(auth.currentUser, { displayName: name })
+            }
+          }
+        ],
+        fields: [
+          {
+            name: 'name',
+            placeholder: 'Ingrese un nombre de usuario',
+          },
+        ],
+      }))
+    })
+  }
 }
