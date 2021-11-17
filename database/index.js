982699d804850c94122d6add9f4638952d35546f
Frank Garcia <00frankgarcia@gmail.com>
2021-11-17 10:52:48 -0300

    fix when asking displayName

--- database/index.js
+++ database/index.js
@@ -9,7 +9,6 @@ export async function createPost(collection, data) {
   try {
     const docRef = await addDoc(useCollection(db, collection), data);
     result = docRef.id;
-    console.log("aca es:", result);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
@@ -19,18 +18,16 @@ export async function createPost(collection, data) {
 
 export async function comprobateDisplayName({ user }) {
   if ("providerData" in user && user.providerData[0].displayName === null) {
-    return new Promise((res) => {
-      const config = {
-        title: 'Como te llamás? 🤔', message: 'Este es el nombre que se mostrara en tus publicacioes (OJO! este no podra cambiarse)',
-        actions: [{
-          text: 'Continuar',
-          onPress: async ({ name }) => {
-            await updateProfile(auth.currentUser, { displayName: name })
-          }
-        }],
-        fields: [{ name: 'name', placeholder: 'Ingrese tu nombre y apellido', }]
-      }
-      res(fire(config));
-    })
+    const config = {
+      title: 'Como te llamás? 🤔', message: 'Este es el nombre que se mostrara en tus publicacioes (OJO! este no podra cambiarse)',
+      actions: [{
+        text: 'Continuar',
+        onPress: async ({ name }) => {
+          await updateProfile(auth.currentUser, { displayName: name, photoURL: "default" })
+        }
+      }],
+      fields: [{ name: 'name', placeholder: 'Ingrese tu nombre y apellido', }]
+    }
+    fire(config);
   }
 }
