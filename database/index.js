e80b5aea2d6500359208c807208aba2b180abd64
Frank Garcia <00frankgarcia@gmail.com>
2021-11-17 23:13:47 -0300

    ++ getPosts method | refactor in database method

--- database/index.js
+++ database/index.js
@@ -1,8 +1,17 @@
-import { collection as useCollection, addDoc } from "firebase/firestore";
-import { updateProfile } from "@firebase/auth";
-import { uploadBytes, getDownloadURL } from "@firebase/storage";
 import { auth, db, storage, ref } from "../services/Firebase";
+import { collection as useCollection, addDoc, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { uploadBytes, getDownloadURL } from "@firebase/storage";
+import { updateProfile } from "@firebase/auth";
 import { fire } from "react-native-alertbox";
+
+export async function getPosts(collection) {
+  const q = query(useCollection(db, collection), orderBy("timestamp", "desc"), limit(6))
+  let posts = [];
+  let snapshot = await getDocs(q);
+  snapshot.docs.forEach(doc => posts.push({ id: doc.id, ...doc.data() }));
+
+  return posts;
+}
 
 export async function createPost(collection, user, imageUri, data) {
   const timestamp = Date.now();
@@ -22,20 +31,18 @@ export async function createPost(collection, user, imageUri, data) {
   return result;
 }
 
-export async function comprobateDisplayName({ user }) {
-  if ("providerData" in user && user.providerData[0].displayName === null) {
-    const config = {
-      title: 'Como te llamás? 🤔', message: 'Este es el nombre que se mostrara en tus publicacioes (OJO! este no podra cambiarse)',
-      actions: [{
-        text: 'Continuar',
-        onPress: async ({ name }) => {
-          await updateProfile(auth.currentUser, { displayName: name, photoURL: "default" })
-        }
-      }],
-      fields: [{ name: 'name', placeholder: 'Ingrese tu nombre y apellido', }]
-    }
-    fire(config);
+export async function changeDisplayName() {
+  const config = {
+    title: 'Como te llamás? 🤔', message: 'Este es el nombre que se mostrara en tus publicacioes (OJO! este no podra cambiarse)',
+    actions: [{
+      text: 'Continuar',
+      onPress: async ({ name }) => {
+        await updateProfile(auth.currentUser, { displayName: name, photoURL: "default" })
+      }
+    }],
+    fields: [{ name: 'name', placeholder: 'Ingrese tu nombre y apellido', }]
   }
+  fire(config);
 }
 
 async function uploadImage(directory, imageUri) {
@@ -56,4 +63,10 @@ async function uploadImage(directory, imageUri) {
   await uploadBytes(ref(storage, directory), blob, { contentType: "image/jpg" })
   let downloadURL = await getDownloadURL(ref(storage, directory));
   return downloadURL;
+}
+
+export default {
+  getPosts,
+  createPost,
+  changeDisplayName
 }
