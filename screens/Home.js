7f0e165ab5d785b8c5ece051244146423d001baa
Frank Garcia <00frankgarcia@gmail.com>
2021-11-17 23:30:28 -0300

    getting posts from firebase

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View } from 'react-native'
+import Database from '../database';
 import ReactNativeModal from 'react-native-modal'
 import PetsList from '../components/PetsList'
 
 export default function Home() {
   const [visible, setVisible] = useState(false);
   const [post, setPost] = useState(null);
+  const [lostPets, setLostPets] = useState([])
+  const [foundPets, setFoundPets] = useState([])
+
+  useEffect(() => {
+    async function getPost() {
+      setLostPets(await Database.getPosts("lostPets"));
+      setFoundPets(await Database.getPosts("foundPets"));
+    }
+    getPost()
+  }, [])
 
   return (
     <View style={{ flex: 1, marginTop: 10 }}>
       {/* Implementar mapa/integracion con google maps?? */}
-      {/* Por mientras, mostrar una lista con los pedidos de busqueda y encontrados */}
-      {post && <ReactNativeModal onBackButtonPress={() => setVisible(false)} children={post} isVisible={visible} />}
-      <PetsList title="Mascotas Perdidas" pets={mascotasPerdidas} setVisible={setVisible} setPost={setPost} />
-      <PetsList title="Mascotas Encontradas" pets={mascotasEncontradas} setVisible={setVisible} setPost={setPost} />
+      {post && <ReactNativeModal onBackButtonPress={() => setVisible(false)} children={post} isVisible={visible} style={{margin: 0}} />}
+      <PetsList title="Mascotas Perdidas" pets={lostPets} setVisible={setVisible} setPost={setPost} />
+      <PetsList title="Mascotas Encontradas" pets={foundPets} setVisible={setVisible} setPost={setPost} />
     </View>
   )
 }
